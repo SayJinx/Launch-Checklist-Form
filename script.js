4fef6fa5beb8e08b20b5840a780e0aa2321b7949
SayJinx <whatsupguest@gmail.com>
2021-06-21 13:21:07 -0500

    Finished planetary functions

--- script.js
+++ script.js
@@ -14,6 +14,9 @@ window.addEventListener("load", function() {
       formCheck();
       //Submit form
       launchReqs();
+
+      //planet data
+      //intGalactic();
    });
    
 //validation
@@ -76,19 +79,46 @@ window.addEventListener("load", function() {
       };
 
       document.getElementById("faultyItems").style.visibility = "visible";
-   }
+   };
+
+   //Planet stuff
+  /* function intGalactic() {
+      fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+         response.json()
+            const missionTarget = document.getElementById("missionTarget");
+               console.log(json[3])
+               missionTarget.innerHTML = `
+               <h2>Mission Destination</h2>
+               <ol>
+                  <li>Name: ${json[3].name}</li>
+                  <li>Diameter: ${json[3].diameter}</li>
+                  <li>Star: ${json[3].star}</li>
+                  <li>Distance from Earth: ${json[3].distance}</li>
+                  <li>Number of Moons: ${json[3].moons}</li>
+               </ol>
+               <img src="${json[3].image}">
+               `;
+            });
+   };*/
+
+         this.fetch("https://handlers.education.launchcode.org/static/planets.json").then(function(response) {
+         response.json().then(function(json) {
+            const missionTarget = document.getElementById("missionTarget");
+               console.log(json[3])
+            missionTarget.innerHTML = `
+            <h2>Mission Destination</h2>
+            <ol>
+               <li>Name: ${json[3].name}</li>
+               <li>Diameter: ${json[3].diameter}</li>
+               <li>Star: ${json[3].star}</li>
+               <li>Distance from Earth: ${json[3].distance}</li>
+               <li>Number of Moons: ${json[3].moons}</li>
+            </ol>
+            <img src="${json[3].image}">
+            `;
+         })
+      })
+
 
 }
 )
-
-/* This block of code shows how to format the HTML once you fetch some planetary JSON!
-<h2>Mission Destination</h2>
-<ol>
-   <li>Name: ${}</li>
-   <li>Diameter: ${}</li>
-   <li>Star: ${}</li>
-   <li>Distance from Earth: ${}</li>
-   <li>Number of Moons: ${}</li>
-</ol>
-<img src="${}">
-*/
