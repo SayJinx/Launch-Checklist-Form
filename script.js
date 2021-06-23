fa97345291f2781e4d52913f28c9024250235acc
SayJinx <whatsupguest@gmail.com>
2021-06-23 11:07:25 -0500

    Attempting changes to feedback

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ window.addEventListener("load", function() {
    const copilotName = document.getElementById("copilotName");
    const fuelLevel = document.getElementById("fuelLevel");
    const cargoMass = document.getElementById("cargoMass");
-   
+   let pilotCheck = false
+   let copilotCheck = false
+   let fuelCheck = false
+   let massCheck = false
+
+
    formSubmit.addEventListener("click", function(event){
       event.preventDefault();
       console.log(event);
@@ -28,18 +33,90 @@ window.addEventListener("load", function() {
       const cargoMassValue = cargoMass.value
    
       //validate fields filled
-      if(pilotNameValue === "" || copilotNameValue === "" || fuelLevelValue === "" || cargoMassValue === "") {
+      if(pilotNameValue === "") {
          alert("All fields are required")
-      };
-      //validate names are not numbers
-      if(!isNaN(pilotNameValue) || !isNaN(copilotNameValue)) {
+         pilotCheck = false
+         //document.getElementById("pilotStatus").innerHTML = `Captain ${pilotNameValue} is not registered for launch.`
+      }
+         else if(!isNaN(pilotNameValue)) {
+            alert("Crew names are not valid")
+            pilotCheck = false
+            //document.getElementById("pilotStatus").innerHTML = `Captain ${pilotNameValue} is not registered for launch.`
+         }
+         else {
+            pilotCheck = true
+         };
+         console.log(pilotCheck)
+
+      if(copilotNameValue === "") {
+         alert("All fields are required")
+         copilotCheck = false
+         //document.getElementById("copilotStatus").innerHTML = `Copilot ${pilotNameValue} is not registered for launch.`
+      }  
+         else if(!isNaN(copilotNameValue)) {
          alert("Crew names are not valid")
+         copilotCheck = false
+         //document.getElementById("pilotStatus").innerHTML = `Captain ${pilotNameValue} is not registered for launch.`
+      }
+         else {
+         copilotCheck = true
+      };
+      console.log(copilotCheck)
+
+      if(fuelLevelValue === "") {
+         alert("All fields are required")
+         fuelCheck = false
+         //document.getElementById("fuelLevel").innerHTML = `Not a valid entry.`
+      }
+         else if(isNaN(fuelLevelValue)) {
+            fuelCheck = false
+         }
+         else if(fuelLevelValue < 10000) {
+            fuelCheck = false
+         }
+         else {
+            fuelCheck = true
+         };
+         console.log(fuelCheck)
+
+      if(cargoMassValue === "") {
+         alert("All fields are required")
+         document.getElementById("fuelLevel").innerHTML = `Not a valid entry.`
+      }
+         else if(isNaN(cargoMassValue)) {
+            massCheck = false
+         }
+         else if(massCheck > 10000) {
+            fuelCheck = false}
+         else {
+            massCheck = true
+         };
+         console.log(massCheck)
+
+
+      //validate names are not numbers
+     /* if(!isNaN(pilotNameValue)) {
+         alert("Crew names are not valid")
+         document.getElementById("pilotStatus").innerHTML = `Captain ${pilotNameValue} is not registered for launch.`
          console.log("hello")
-      };
-      if(isNaN(fuelLevelValue) || isNaN(cargoMassValue)) {
+      };*/
+
+     /* if(!isNaN(copilotNameValue)) {
+         alert("Crew names are not valid")
+         document.getElementById("copilotStatus").innerHTML = `Copilot ${pilotNameValue} is not registered for launch.`
+         console.log("hello")*/
+
+      /*if(isNaN(fuelLevelValue) || isNaN(cargoMassValue)) {
          alert("Fuel Level and Cargo Mass must be numbers")
+         document.getElementById("fuelLevel").innerHTML = `Fuel Level not a valid entry.`
+      };*/
+
+      /*if(isNaN(cargoMassValue)) {
+         alert("Fuel Level and Cargo Mass must be numbers")
+         document.getElementById("cargoMass").innerHTML = `Cargo Mass not a valid entry.`
       };
-   };
+   };*/
+};
 
 //faulty items
 
@@ -50,13 +127,23 @@ window.addEventListener("load", function() {
       const cargoMassValue = cargoMass.value
 
       //update pilot
+      if(pilotCheck = true) {
       document.getElementById("pilotStatus").innerHTML = `Captain ${pilotNameValue} is registered and ready for launch.`
+      }
+         else if(pilotCheck = false) {
+            document.getElementById("pilotStatus").innerHTML = `Captain ${pilotNameValue} is NOT registered and ready for launch.`
+         };
 
       //update copilot
+      if(copilotCheck = true) {
       document.getElementById("copilotStatus").innerHTML = `Co-Pilot ${copilotNameValue} is registered and ready for launch.`
-
+      }
+         else if(copilotCheck = false) {
+            document.getElementById("pilotStatus").innerHTML = `Co-Pilot ${pilotNameValue} is NOT registered and ready for launch.`
+         };
+      
       //check fuel
-      if(fuelLevelValue < 10000) {
+      /*if(fuelLevelValue < 10000) {
          document.getElementById("fuelStatus").innerHTML = `Not enough fuel for journey`
          document.getElementsByClassName("h2").innerHTML = `Shuttle is not ready for launch`
       }
@@ -76,7 +163,18 @@ window.addEventListener("load", function() {
          document.getElementById("cargoStatus").innerHTML = `Cargo mass low enough for launch`;
          document.getElementById("launchStatus").innerHTML = `Shuttle is ready for launch!`
          document.getElementById("launchStatus").style.color = "green";
-      };
+      };*/
+
+      if(pilotCheck === true && copilotCheck === true && fuelCheck === true && massCheck === true) {
+         document.getElementById("launchStatus").innerHTML = `Shuttle is ready for launch!`
+         document.getElementById("launchStatus").style.color = "green";
+         console.log("Check complete")
+      }
+         else {
+            document.getElementById("launchStatus").innerHTML = `Shuttle is not ready for launch`;
+            document.getElementById("launchStatus").style.color = "red";
+            console.log("Check failed")
+         }
 
       document.getElementById("faultyItems").style.visibility = "visible";
    };
